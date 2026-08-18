c9e7947b1bca09a6a889452660f3a483eea8ac09
Thế Long <73347418+the-long-ride@users.noreply.github.com>
2026-08-18 14:13:15 +0700

    fix(windows): register APO through ATL object map

--- native/windows/apo/voxveil_apo.h
+++ native/windows/apo/voxveil_apo.h
@@ -13,11 +13,14 @@ extern const CLSID CLSID_VoxveilApo;
 
 class ATL_NO_VTABLE CVoxveilApo :
     public CComObjectRootEx<CComMultiThreadModel>,
+    public CComCoClass<CVoxveilApo, &CLSID_VoxveilApo>,
     public CBaseAudioProcessingObject,
     public IAudioSystemEffects {
 public:
     CVoxveilApo();
     ~CVoxveilApo() override = default;
+
+    DECLARE_NO_REGISTRY()
 
     BEGIN_COM_MAP(CVoxveilApo)
         COM_INTERFACE_ENTRY(IAudioProcessingObject)
@@ -25,6 +28,8 @@ public:
         COM_INTERFACE_ENTRY(IAudioProcessingObjectConfiguration)
         COM_INTERFACE_ENTRY(IAudioSystemEffects)
     END_COM_MAP()
+
+    DECLARE_PROTECT_FINAL_CONSTRUCT()
 
     STDMETHOD(Initialize)(UINT32 cb_data_size, BYTE* data) override;
     STDMETHOD_(void, APOProcess)(
@@ -45,3 +50,5 @@ private:
     voxveil::ControlState control_;
     std::uint32_t channels_{2};
 };
+
+OBJECT_ENTRY_AUTO(CLSID_VoxveilApo, CVoxveilApo)
